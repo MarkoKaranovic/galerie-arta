e49a38a41e39b953024a73b213ae01f7654a2b0b
markokaranovic <marko.karanovic@fronttribe.com>
2023-06-09 15:27:14 +0200

    feat: fix build

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,12 +1,11 @@
 import { Box, Container } from '@mantine/core';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import * as sdp from 'sdpoker';
+
 import Footer from '../Footer';
 import Navigation from '../Navigation';
 
 export default function Layout() {
-  console.log(sdp);
   return (
     <Container
       style={{ border: '1px solid red', overflow: 'hidden' }}
